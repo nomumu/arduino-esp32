299d0d7bd8cad0fe1c670348f1bc02a4630901e9
nomumu <nomumu-github@koso2-dan.ddo.jp>
2021-07-24 05:52:57 +0900

    Bug fix

--- libraries/WiFi/src/ETH.cpp
+++ libraries/WiFi/src/ETH.cpp
@@ -288,7 +288,11 @@ bool ETHClass::begin(uint8_t phy_addr, int power, int mdc, int mdio, eth_phy_typ
             break;
 #endif
         case ETH_PHY_KSZ8081:
+#if 0 /* bug fix for library */
             eth_phy = esp_eth_phy_new_ksz8081(&phy_config);
+#else
+            eth_phy = esp_eth_phy_new_ksz8041(&phy_config);
+#endif
         default:
             break;
     }
